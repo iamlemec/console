44f54dd74dae93c0ade2f91fa391cb3e37ae1f7d
Doug Hanley <doug@castor.castor.com>
2013-01-14 22:33:24 -0500

    stage 1 complete

--- console.py
+++ console.py
@@ -23,20 +23,26 @@ def rand_walk(n):
 
 n_base = 1024
 theta = np.linspace(0.0,2.0*np.pi,n_base)
-upd_speed = 0.01
+randw = rand_walk(n_base)
+upd_speed = 0.03
 
 field_types = {'position':float,'velocity':float}
-field_vals = dict([(f,t()) for (f,t) in field_types.items()])
 field_names = field_types.keys()
+field_vals = {'position':0.0,'velocity':0.03}
 
 plot_types = {'data1':n_base,'data2':n_base}
 plot_names = plot_types.keys()
-plot_xvals = dict([(pname,np.linspace(0.0,2.0*np.pi,n)) for (pname,n) in plot_types.items()])
-plot_yvals = dict([(pname,np.zeros(n)) for (pname,n) in plot_types.items()])
+plot_xvals = {'data1':np.linspace(0.0,2.0*np.pi,n_base),'data2':np.linspace(0.0,2.0*np.pi,n_base)}
+plot_yvals = {'data1':np.zeros(n_base),'data2':np.zeros(n_base)}
 
+# zmq interface
 context = zmq.Context()
-socket = context.socket(zmq.PUB)
-socket.bind("tcp://0.0.0.0:6123")
+socket_out = context.socket(zmq.PUB)
+socket_out.bind("tcp://0.0.0.0:6123")
+
+socket_in = context.socket(zmq.SUB)
+socket_in.connect("tcp://0.0.0.0:6124")
+socket_in.setsockopt(zmq.SUBSCRIBE,"")
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -61,11 +67,12 @@ class DataHandler(tornado.websocket.WebSocketHandler):
 
     def open(self):
         print "connection received"
-        ioloop = tornado.ioloop.IOLoop.instance()
-        ioloop.add_timeout(timedelta(seconds=0.1),self.update_plots)
+        self.do_update = True
+        self.update_plots()
 
     def on_close(self):
         print "connection closing"
+        self.do_update = False
 
     def error_msg(self, error_code):
         if not error_code is None:
@@ -77,7 +84,7 @@ class DataHandler(tornado.websocket.WebSocketHandler):
 
     def on_message(self, message):
         print "received message: {0}".format(message)
-        socket.send_unicode(message)
+        socket_out.send_unicode(message)
 
         json_data = json.loads(message)
         cmd = json_data.get('cmd','')
@@ -85,22 +92,19 @@ class DataHandler(tornado.websocket.WebSocketHandler):
 
         if cmd == 'store_field':
             value = json_data.get('value','')
-            if name in vals:
-                print 'Storing %s in %s' % (value,field)
+            if name in field_names:
+                print 'Storing %s=%s' % (name,value)
                 try:
-                    value = types[name](value) 
-                    field_vals[name] = value
-                    json_out = {'cmd':'update_field','name':name,'value':str(value)}
-                    self.write_message(json.dumps(json_out))
+                    field_vals[name] = field_types[name](value)
+                    self.send_field(name)
                 except Exception, e:
                     print 'Invalid value \'%s\' for field %s' % (value,name)
+                    print e
             else:
                 print 'Invalid field %s' % (name,)
         elif cmd == 'fetch_field':
             if name in field_names:
-                value = field_vals[name]
-                json_out = {'cmd':'update_field','name':name,'value':str(value)}
-                self.write_message(json.dumps(json_out))
+                self.send_field(name)
             else:
                 print 'Invalid field %s' % (name,)
         elif cmd == 'fetch_plot':
@@ -112,16 +116,25 @@ class DataHandler(tornado.websocket.WebSocketHandler):
     def update_plots(self):
         global theta
 
-        theta = theta + 0.1
+        theta += field_vals['velocity']
         theta[theta>2.0*np.pi] -= 2.0*np.pi
 
-        plot_yvals['data1'] = np.sin(theta)
-        plot_yvals['data2'] = np.cos(2.0*theta)
-        self.send_plot('data1')
-        self.send_plot('data2')
+        randw[:-10] = randw[10:]
+        randw[-10:] = rand_walk(10) + randw[-10]
+
+        if self.do_update:
+          plot_yvals['data1'] = np.sin(theta) + field_vals['position']
+          plot_yvals['data2'] = randw
+          self.send_plot('data1')
+          self.send_plot('data2')
 
         ioloop = tornado.ioloop.IOLoop.instance()
         ioloop.add_timeout(timedelta(seconds=upd_speed),self.update_plots)
+
+    def send_field(self,fname):
+        value = field_vals[fname]
+        json_out = {'cmd':'update_field','name':fname,'value':  value}
+        self.write_message(json.dumps(json_out))
 
     def send_plot(self,pname):
         x_values = plot_xvals[pname]
